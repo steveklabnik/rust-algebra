3d67ea654db4575eca0deb43b888fe7037d8beb9
Darin Morrison <darinmorrison+git@gmail.com>
2014-10-29 04:23:56 -0600

    Add documentation

---
+++ doc/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['algebra'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='algebra/structure/and/struct.And.html' title='algebra::structure::and::And'>And</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='algebra/structure/or/struct.Or.html' title='algebra::structure::or::Or'>Or</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/core/ops/trait.Mul.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['algebra'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;A&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Mul.html' title='core::ops::Mul'>Mul</a>&lt;<a class='struct' href='algebra/magma/theory/struct.M.html' title='algebra::magma::theory::M'>M</a>&lt;A&gt;, <a class='struct' href='algebra/magma/theory/struct.M.html' title='algebra::magma::theory::M'>M</a>&lt;A&gt;&gt; for <a class='struct' href='algebra/magma/theory/struct.M.html' title='algebra::magma::theory::M'>M</a>&lt;A&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
